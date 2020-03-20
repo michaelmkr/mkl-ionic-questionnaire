ef2608a9fc699f085eb66c331977543a809a0526
MichaelMkr <michael.macher@humanizing.com>
2020-03-20 17:14:46 +0100

    added a button that links to question-list

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {DataService} from '../../bo/data.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  public ausgabe: string;
-  constructor(private data: DataService) {
-    this.ausgabe = this.data.getText();
+    public ausgabe: string;
+
+    constructor(private data: DataService, private router: Router) {
+        this.ausgabe = this.data.getText();
+    }
+
+  public showList() {
+      this.router.navigateByUrl('/question-list');
   }
 
 }
