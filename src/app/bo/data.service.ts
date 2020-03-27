36856f49f94a895e6cbacfda724968c44f5846ef
MichaelMkr <michael.macher@humanizing.com>
2020-03-27 17:03:27 +0100

    added load and save functions for local storage

--- src/app/bo/data.service.ts
+++ src/app/bo/data.service.ts
@@ -1,6 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Question, Quiz} from './interfaces';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
+import {Plugins} from '@capacitor/core';
+
+const {Storage} = Plugins;
+
 
 @Injectable({
     providedIn: 'root'
@@ -8,23 +12,33 @@ import { v4 as uuidv4 } from 'uuid';
 export class DataService {
 
     constructor() {
+        this.load();
+        // this.currentQuiz.questions.push({
+        //     id: uuidv4(),
+        //     title: 'How much equals 1+2?',
+        //     a1: '1',
+        //     a2: '2',
+        //     a3: '3',
+        //     a4: '4',
+        //     correct: 3
+        // });
+        // this.currentQuiz.questions.push({
+        //     id: uuidv4(),
+        //     title: 'How much equals 2+2?',
+        //     a1: '1',
+        //     a2: '2',
+        //     a3: '3',
+        //     a4: '4',
+        //     correct: 4
+        // });
         this.currentQuiz.questions.push({
             id: uuidv4(),
-            title: 'How much equals 1+2?',
-            a1: '1',
-            a2: '2',
-            a3: '3',
-            a4: '4',
-            correct: 3
-        });
-        this.currentQuiz.questions.push({
-            id: uuidv4(),
-            title: 'How much equals 2+2?',
-            a1: '1',
-            a2: '2',
-            a3: '3',
-            a4: '4',
-            correct: 4
+            title: 'Initial Question Fallback if Storage empty',
+            a1: '',
+            a2: '',
+            a3: '',
+            a4: '',
+            correct: 1
         });
     }
 
@@ -68,9 +82,23 @@ export class DataService {
         this.save();
     }
 
-    public load() {
+    public async load() { // Grischa mag keine Promises mit .then
+        try {
+            const res = await Storage.get({key: 'myQuestions'});
+            if (res.value) {
+                this.currentQuiz = JSON.parse(res.value);
+            } else {
+                console.log('res.value does not exist');
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     public save() {
+        Storage.set({
+            key: 'myQuestions',
+            value: JSON.stringify(this.currentQuiz),
+        });
     }
 }
